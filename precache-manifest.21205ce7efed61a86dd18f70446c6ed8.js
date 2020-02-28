13261d4a77d9fba8f093b97ea51c75a64d5ad6cf
ssuchir <ssuchir@gmail.com>
2020-02-28 18:16:18 -0500

    Updates

---
+++ precache-manifest.21205ce7efed61a86dd18f70446c6ed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06599f5c3542629d1dfdccd8cf430ff5",
+    "url": "/react-redux-blog/index.html"
+  },
+  {
+    "revision": "dd6cb405163fb33f0c40",
+    "url": "/react-redux-blog/static/js/2.79114585.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/react-redux-blog/static/js/2.79114585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83aa3d673b6d9aa06b4f",
+    "url": "/react-redux-blog/static/js/main.87a30fd2.chunk.js"
+  },
+  {
+    "revision": "a8475864ad76a726cf53",
+    "url": "/react-redux-blog/static/js/runtime-main.a446e273.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.194d357af81301dd9fe8cfb7835b485d.js"
+  "/react-redux-blog/precache-manifest.21205ce7efed61a86dd18f70446c6ed8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
