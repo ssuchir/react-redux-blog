9b9e5c05a4e10353513c2d74775749fae40d9429
ssuchir <ssuchir@gmail.com>
2020-02-28 18:13:22 -0500

    Updates

---
+++ precache-manifest.194d357af81301dd9fe8cfb7835b485d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ac477c7fdb3c480c9f275afaa4ceb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd6cb405163fb33f0c40",
+    "url": "/static/js/2.79114585.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/static/js/2.79114585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83aa3d673b6d9aa06b4f",
+    "url": "/static/js/main.87a30fd2.chunk.js"
+  },
+  {
+    "revision": "f34df4cc19e0da90cba3",
+    "url": "/static/js/runtime-main.578b6095.js"
+  }
+]);
